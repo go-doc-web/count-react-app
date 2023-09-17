0e7041cd0fa9f090d9af15701b7071d726900d98
Oleh Hubskiy <gubskyolegod@gmail.com>
2023-09-17 19:50:42 +0300

    end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import '../index.scss';
+import { useState } from 'react';
+
 export const App = () => {
+  const [count, setCount] = useState(0);
+  // let count = 0;
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  };
+  const onClickMinus = () => {
+    setCount(count - 1);
+  };
+
+  const onClickReset = () => {
+    setCount(count * 0);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">
+          - Минус
+        </button>
+        <button onClick={onClickPlus} className="plus">
+          Плюс +
+        </button>
+        <button onClick={onClickReset} className="reset">
+          reset
+        </button>
+      </div>
     </div>
   );
 };
